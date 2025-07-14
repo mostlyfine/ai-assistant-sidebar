1029acdd9db57dcaf5b4b4be2b0f9a48cb57ec43
mostlyfine <mostlyfine@gmail.com>
2025-07-15 01:41:06 +0900

    fix(jest): Jestのカバレッジ計測設定を修正

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  rootDir: '.',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'scripts/**/*.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
+};
